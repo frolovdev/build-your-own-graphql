89c64fbd82e901caff2deb5d41399c2174afbef2
frolovdev <frolovandrey752@gmail.com>
2022-10-14 19:15:23 +0400

    change tests to handle locations

--- src/lexer/__tests__/lexer.test.ts
+++ src/lexer/__tests__/lexer.test.ts
@@ -7,14 +7,26 @@ test('basic test', () => {
   const lexer = new Lexer(input);
 
   expect(lexer.token.kind).toEqual(TokenKind.SOF);
-  expect(lexer.advance().kind).toEqual(TokenKind.BRACE_L);
-  expect(lexer.advance().kind).toEqual(TokenKind.BRACE_R);
-  expect(lexer.advance().kind).toEqual(TokenKind.PAREN_L);
-  expect(lexer.advance().kind).toEqual(TokenKind.PAREN_R);
-  expect(lexer.advance().kind).toEqual(TokenKind.BANG);
-  expect(lexer.advance().kind).toEqual(TokenKind.QUESTION_MARK);
-  expect(lexer.advance().kind).toEqual(TokenKind.EOF);
-  expect(lexer.advance().kind).toEqual(TokenKind.EOF);
+  expect(lexOne(lexer)).toEqual({
+    column: 1,
+    end: 1,
+    kind: TokenKind.BRACE_L,
+    line: 1,
+    start: 0,
+  });
+  expect(lexOne(lexer)).toEqual({ column: 2, end: 2, kind: TokenKind.BRACE_R, line: 1, start: 1 });
+  expect(lexOne(lexer)).toEqual({ column: 3, end: 3, kind: TokenKind.PAREN_L, line: 1, start: 2 });
+  expect(lexOne(lexer)).toEqual({ column: 4, end: 4, kind: TokenKind.PAREN_R, line: 1, start: 3 });
+  expect(lexOne(lexer)).toEqual({ column: 5, end: 5, kind: TokenKind.BANG, line: 1, start: 4 });
+  expect(lexOne(lexer)).toEqual({
+    column: 6,
+    end: 6,
+    kind: TokenKind.QUESTION_MARK,
+    line: 1,
+    start: 5,
+  });
+  expect(lexOne(lexer)).toEqual({ column: 7, end: 6, kind: TokenKind.EOF, line: 1, start: 6 });
+  expect(lexOne(lexer)).toEqual({ column: 7, end: 6, kind: TokenKind.EOF, line: 1, start: 6 });
 });
 
 test('lexer handles whitespaces', () => {
@@ -22,15 +34,24 @@ test('lexer handles whitespaces', () => {
 
   const lexer = new Lexer(input);
 
-  expect(lexer.token.kind).toEqual(TokenKind.SOF);
-  expect(lexer.advance().kind).toEqual(TokenKind.BRACE_L);
-  expect(lexer.advance().kind).toEqual(TokenKind.BRACE_R);
-  expect(lexer.advance().kind).toEqual(TokenKind.PAREN_L);
-  expect(lexer.advance().kind).toEqual(TokenKind.PAREN_R);
-  expect(lexer.advance().kind).toEqual(TokenKind.BANG);
-  expect(lexer.advance().kind).toEqual(TokenKind.QUESTION_MARK);
-  expect(lexer.advance().kind).toEqual(TokenKind.EOF);
-  expect(lexer.advance().kind).toEqual(TokenKind.EOF);
+  expect(lexOne(lexer)).toEqual({
+    column: 1,
+    end: 1,
+    kind: TokenKind.BRACE_L,
+    line: 1,
+    start: 0,
+  });
+  expect(lexOne(lexer)).toEqual({ column: 2, end: 2, kind: TokenKind.BRACE_R, line: 1, start: 1 });
+  expect(lexOne(lexer)).toEqual({ column: 5, end: 5, kind: TokenKind.PAREN_L, line: 1, start: 4 });
+  expect(lexOne(lexer)).toEqual({ column: 6, end: 6, kind: TokenKind.PAREN_R, line: 1, start: 5 });
+  expect(lexOne(lexer)).toEqual({ column: 10, end: 10, kind: TokenKind.BANG, line: 1, start: 9 });
+  expect(lexOne(lexer)).toEqual({
+    column: 11,
+    end: 11,
+    kind: TokenKind.QUESTION_MARK,
+    line: 1,
+    start: 10,
+  });
 });
 
 test('lexer handles new line', () => {
@@ -38,9 +59,18 @@ test('lexer handles new line', () => {
 
   const lexer = new Lexer(input);
 
-  expect(lexer.token.kind).toEqual(TokenKind.SOF);
-  expect(lexer.advance().kind).toEqual(TokenKind.BRACE_L);
-  expect(lexer.advance().kind).toEqual(TokenKind.BRACE_R);
-  expect(lexer.advance().kind).toEqual(TokenKind.PAREN_L);
-  expect(lexer.advance().kind).toEqual(TokenKind.PAREN_R);
+  expect(lexOne(lexer)).toEqual({
+    column: 1,
+    end: 1,
+    kind: TokenKind.BRACE_L,
+    line: 1,
+    start: 0,
+  });
+  expect(lexOne(lexer)).toEqual({ column: 2, end: 2, kind: TokenKind.BRACE_R, line: 1, start: 1 });
+  expect(lexOne(lexer)).toEqual({ column: 2, end: 5, kind: TokenKind.PAREN_L, line: 2, start: 4 });
+  expect(lexOne(lexer)).toEqual({ column: 3, end: 6, kind: TokenKind.PAREN_R, line: 2, start: 5 });
 });
+
+function lexOne(lexer: Lexer) {
+  return lexer.advance().toJSON();
+}
