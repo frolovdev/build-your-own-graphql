4b105d3b861af678670577bacf0aa0d918fca0b9
frolovdev <frolovandrey752@gmail.com>
2022-10-17 19:43:52 +0400

    add location

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -1,3 +1,48 @@
+import { Token } from '../lexer/token';
+
+export class Location {
+  /**
+   * The character offset at which this Node begins.
+   */
+  readonly start: number;
+
+  /**
+   * The character offset at which this Node ends.
+   */
+  readonly end: number;
+
+  /**
+   * The Token at which this Node begins.
+   */
+  readonly startToken: Token;
+
+  /**
+   * The Token at which this Node ends.
+   */
+  readonly endToken: Token;
+
+  /**
+   * The Source document the AST represents.
+   */
+  readonly source: string;
+
+  constructor(startToken: Token, endToken: Token, source: string) {
+    this.start = startToken.start;
+    this.end = endToken.end;
+    this.startToken = startToken;
+    this.endToken = endToken;
+    this.source = source;
+  }
+
+  get [Symbol.toStringTag]() {
+    return 'Location';
+  }
+
+  toJSON(): { start: number; end: number } {
+    return { start: this.start, end: this.end };
+  }
+}
+
 export enum Kind {
   NAME = 'Name',
   DOCUMENT = 'Document',
